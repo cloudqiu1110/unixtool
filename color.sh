61bb2baeb33dc7bd982c60811e34526435aa84d9
cloudqiu <cloudqiu1110@gmail.com>
2014-09-26 17:41:25 +0800

    Create color.sh

---
+++ color.sh
@@ -0,0 +1,20 @@
+#! /usr/bin/python
+
+NORMAL=$(tput sgr0)
+GREEN=$(tput setaf 2; tput bold)
+YELLOW=$(tput setaf 3)
+RED=$(tput setaf 1)
+
+function red() {
+	echo -e "$RED$*$NORMAL"
+}
+
+function green() {
+	echo -e "$GREEN$*$NORMAL"
+}
+
+function yello() {
+	echo -e "$YELLO$*$NORMAL"
+}
+
+red "sdfhit"
